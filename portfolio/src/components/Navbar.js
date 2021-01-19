f29d601003f519692c6ad084b28ced1f0177aa3f
Bradley Wilson <bluedirtbike6@gmail.com>
2021-01-19 16:28:09 -0600

    add sticky nav feature

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -5,12 +5,14 @@ class Navbar extends Component {
     state = {
         isActive: false,
         h1className: "",
-        ulclassName: "nav-links"
+        ulclassName: "nav-links",
+        navOffsetTop: 0
     }
 
     toggleNav = () => {
         this.setState({isActive: !this.state.isActive}, this.addClass)
     }
+
     addClass = () => {
         if (this.state.isActive){
             this.setState({h1className: "nav-links-active", ulclassName: "nav-links nav-links-active-animation"})
@@ -18,6 +20,27 @@ class Navbar extends Component {
             this.setState({h1className: "", ulclassName: "nav-links"})
         }
     }
+
+    stickyNav = (sticky, navbar) => {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+    }
+
+    onWindowResize = (navbar) => {
+        console.log("hi")
+        this.setState({navOffsetTop: navbar.offsetTop})
+    }
+
+    componentDidMount(){
+        let navbar = document.getElementById('nav')
+        this.setState({navOffsetTop: navbar.offsetTop})
+        window.addEventListener("scroll", () => this.stickyNav(this.state.navOffsetTop, navbar), false)
+        window.addEventListener("resize", () => this.onWindowResize(navbar), false)
+    }
+    
     render() {
         console.log(this.state)
         return (
