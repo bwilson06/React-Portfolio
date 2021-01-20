ca6bef395d962d5312bb30234dd579622d2114a6
Bradley Wilson <bluedirtbike6@gmail.com>
2021-01-19 20:45:01 -0600

    fix threejs overflow issue

--- src/components/Animaton.js
+++ src/components/Animaton.js
@@ -7,7 +7,7 @@ class Animaton extends Component {
 
     init = () => {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.prepend(renderer.domElement );
