99d858f430a2f4033559a774978255ba7393be0a
Bradley Wilson <bluedirtbike6@gmail.com>
2021-01-20 14:22:23 -0600

    yoyo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ class Navbar extends Component {
     stickyNav = (sticky, navbar) => {
         console.log(sticky[0].clientHeight)
         console.log(window.pageYOffset, this.state.canvasHeight)
-        if (window.pageYOffset >= sticky[0].clientHeight -10) {
+        if (window.pageYOffset >= sticky[0].clientHeight -100) {
             navbar.classList.add("sticky")
           } else {
             navbar.classList.remove("sticky")
