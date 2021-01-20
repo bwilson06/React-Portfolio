49b335fbd95e76a3345013d96bc543df0658c37f
Bradley Wilson <bluedirtbike6@gmail.com>
2021-01-20 10:05:17 -0600

    idk

--- src/components/Animaton.js
+++ src/components/Animaton.js
@@ -33,9 +33,6 @@ class Animaton extends Component {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.prepend(renderer.domElement);
     renderer.domElement.classList.add("canvas");
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    renderer.domElement.style.setProperty('--vh', `${vh}px`)
     const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
     const edges = new THREE.EdgesGeometry(geometry);
     lineColor = 0xffffff;
@@ -95,9 +92,6 @@ class Animaton extends Component {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     this.setState({ windowHeight: window.innerHeight });
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
   };
 
   scrollToBottom = () => {
