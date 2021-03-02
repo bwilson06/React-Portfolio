01d95c389194ea19f7eb6403f3dd481cae30508f
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 22:31:23 -0600

    fix heroku path error.

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 }
 
 var transporter = nodemailer.createTransport({
