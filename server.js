b6a5ef4ffb11208770c22412e1cb0a9a81daf1f7
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 21:58:02 -0600

    fix path error

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.use(express.static(path.join(__dirname, './client/build')))
   // Anything that doesn't match the above, send back index.html
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + './client/build/index.html'))
   })
 }
 
