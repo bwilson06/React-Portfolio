e0f2311e985c0bc3c01d31162de35f4ab52e7666
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-02 12:29:13 -0600

    idk anymore

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 require('dotenv').config()
-const path = require('path')
-
-let PORT = 3001 || process.env.PORT
 
 let app = express();
+
+let PORT = 3001 || process.env.PORT
 
 //parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')))
-  // Anything that doesn't match the above, send back index.html
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-  }) 
-  
-  }
-
-//making public a static folder
-app.use(express.static("public"));
+  app.use(express.static('client/build'))
+}
 
 var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
