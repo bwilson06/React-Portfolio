9f6d3d22a90f9bbac4f325df8ceed635ee418a0d
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 22:36:24 -0600

    heroku path error

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 var transporter = nodemailer.createTransport({
