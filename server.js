20a5e004342fc9db21e311f7358845e9f0d8e53a
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 22:56:09 -0600

    idk

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')))
-  // Anything that doesn't match the above, send back index.html
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-  })
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 var transporter = nodemailer.createTransport({
