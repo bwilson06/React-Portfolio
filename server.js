4b869b48b40a279b72756fa61ca58f9d5e450a77
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 22:15:54 -0600

    fix path issue

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"))
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 var transporter = nodemailer.createTransport({
