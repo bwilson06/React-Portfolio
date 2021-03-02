8bb562e9660c722073a7f464022da72c430f8c5f
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-01 22:40:22 -0600

    heroku error

--- server.js
+++ server.js
@@ -13,13 +13,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use(express.static('dist'));
+  app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 }
 
 var transporter = nodemailer.createTransport({
