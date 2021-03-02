95213505b329e4a1110568851984aa822145a9f3
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-02 14:16:10 -0600

    sdsdsdsd

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
+  
 }
 
 var transporter = nodemailer.createTransport({
