8fb0846040a4c08500f3f9df72968dd69db2b2db
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-03 13:01:36 -0600

    sdsd

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use(express.json());
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-  
 }
 
 var transporter = nodemailer.createTransport({
