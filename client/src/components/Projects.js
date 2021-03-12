4ee55eae9c52062abf781a09081649b3c91347d8
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-11 21:59:06 -0600

    add link to dogstr on carousel

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Carousel} from "react-bootstrap";
+import { Container, Carousel } from "react-bootstrap";
 import dogstr from "../images/dogstr.png";
 import chatty from "../images/chatty.png";
 import petsfurfriends from "../images/PetsFurFriends.png";
@@ -11,9 +11,9 @@ const Projects = () => {
   };
 
   const scroll = (e) => {
-      e.preventDefault()
-      document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
-  }
+    e.preventDefault();
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <div id="projects" className="projects">
@@ -27,31 +27,51 @@ const Projects = () => {
         </div>
         <Carousel id="carousel" activeIndex={index} onSelect={handleSelect}>
           <Carousel.Item>
-            <img className="d-block carousel-image" src={dogstr} alt="First slide" />
-            <Carousel.Caption>
-              <h3>Dogstr</h3>
-              <p>Search for top rated Dog Parks in your area.</p>
-            </Carousel.Caption>
+            <a href="https://dogstr-react.herokuapp.com/" target="_blank">
+              <img
+                className="d-block carousel-image"
+                src={dogstr}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Dogstr</h3>
+                <p>Search for top rated Dog Parks in your area.</p>
+              </Carousel.Caption>
+            </a>
           </Carousel.Item>
           <Carousel.Item>
             <a href="https://chatty-react-bw.herokuapp.com/" target="_blank">
-            <img className="d-block carousel-image" src={chatty} alt="Second slide" />
-            <Carousel.Caption>
-              <h3>Chatty</h3>
-              <p>Instantly connect/chat with people around the world.</p>
-            </Carousel.Caption>
+              <img
+                className="d-block carousel-image"
+                src={chatty}
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>Chatty</h3>
+                <p>Instantly connect/chat with people around the world.</p>
+              </Carousel.Caption>
             </a>
           </Carousel.Item>
           <Carousel.Item>
-            <a href="https://team-crush-it.github.io/Pets-Fur-Friends/" target="_blank"><img className="d-block carousel-image" src={petsfurfriends} alt="Third slide" />
-            <Carousel.Caption>
-              <h3>Pets Fur Friends</h3>
-              <p>Looking to adopt? Find the purrfect pet for you!</p>
-            </Carousel.Caption>
+            <a
+              href="https://team-crush-it.github.io/Pets-Fur-Friends/"
+              target="_blank"
+            >
+              <img
+                className="d-block carousel-image"
+                src={petsfurfriends}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Pets Fur Friends</h3>
+                <p>Looking to adopt? Find the purrfect pet for you!</p>
+              </Carousel.Caption>
             </a>
           </Carousel.Item>
         </Carousel>
-        <h1 onClick={(e) => scroll(e)} className="projects-scroll"><i class="fas fa-arrow-circle-down"></i></h1>
+        <h1 onClick={(e) => scroll(e)} className="projects-scroll">
+          <i class="fas fa-arrow-circle-down"></i>
+        </h1>
       </Container>
     </div>
   );
